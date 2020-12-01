bd40a28ee19e10b956ac4b0a980efb4f61904be3
LLIYY <1@1.cn>
2020-12-02 00:43:38 +0800

    asd

--- src/logged_out/components/navigation/NavBar.js
+++ src/logged_out/components/navigation/NavBar.js
@@ -71,6 +71,7 @@ function NavBar(props) {
       icon: <LockOpenIcon className="text-white" />
     }
   ];
+  console.log(classes)
   return (
     <div className={classes.root}>
       <AppBar position="fixed" className={classes.appBar}>
@@ -117,7 +118,7 @@ function NavBar(props) {
                         color="secondary"
                         size="large"
                         classes={{ text: classes.menuButtonText }}
-                      >
+                      >asd
                         {element.name}
                       </Button>
                     </Link>
